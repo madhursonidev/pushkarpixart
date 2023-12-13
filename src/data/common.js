8c0a4b1b685e2aceff6d5932f803531ff7703ccc
madhursonidev <madhursonidev@gmail.com>
2023-12-14 01:05:35 +0530

    Worked on animated arrow and responsiveness

---
+++ src/data/common.js
@@ -0,0 +1,73 @@
+export const photos = [
+  {
+    id: 1,
+    title: "Sunset by the Beach",
+    imageUrl:
+      "https://images.unsplash.com/photo-1702234577513-151004f4cb44?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "A breathtaking view of the sunset over the ocean.",
+  },
+  {
+    id: 2,
+    title: "Cityscape at Night",
+    imageUrl:
+      "https://images.unsplash.com/photo-1699775292727-06fabf36730d?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Vibrant city lights illuminating the night sky.",
+  },
+  {
+    id: 3,
+    title: "Mountain View",
+    imageUrl:
+      "https://images.unsplash.com/photo-1556912179-1357f70f8a08?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Majestic view of mountains and clouds.",
+  },
+  {
+    id: 4,
+    title: "Serenity in Nature",
+    imageUrl:
+      "https://images.unsplash.com/photo-1560184897-1e531a8321a2?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Peaceful scene of nature with lush greenery.",
+  },
+  {
+    id: 5,
+    title: "Urban Skyline",
+    imageUrl:
+      "https://images.unsplash.com/photo-1493809842364-78817add7ffb?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Stunning view of a modern urban skyline.",
+  },
+  {
+    id: 6,
+    title: "Misty Forest",
+    imageUrl:
+      "https://images.unsplash.com/photo-1445282768818-728615cc910a?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Mysterious and misty forest landscape.",
+  },
+  {
+    id: 7,
+    title: "Rural Countryside",
+    imageUrl:
+      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Scenic view of the peaceful rural countryside.",
+  },
+  {
+    id: 8,
+    title: "Ocean Sunset",
+    imageUrl:
+      "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Golden hues of sunset reflecting on the ocean waves.",
+  },
+  {
+    id: 9,
+    title: "Ethereal Clouds",
+    imageUrl:
+      "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Cloudscape painting a dreamy picture in the sky.",
+  },
+  {
+    id: 10,
+    title: "Winter Wonderland",
+    imageUrl:
+      "https://images.unsplash.com/photo-1487941651266-6f74803149b1?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Snow-covered landscape creating a magical scene.",
+  },
+  // ... Add more objects as needed
+];
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,33 +5,22 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import AboutUs from "src/components/AboutUs";
 import { IoIosArrowDown } from "react-icons/io";
-
-const photos = [
-  {
-    id: 1,
-    title: "Sunset by the Beach",
-    imageUrl:
-      "https://images.unsplash.com/photo-1702234577513-151004f4cb44?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    description: "A breathtaking view of the sunset over the ocean.",
-  },
-  {
-    id: 2,
-    title: "Cityscape at Night",
-    imageUrl:
-      "https://images.unsplash.com/photo-1699775292727-06fabf36730d?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    description: "Vibrant city lights illuminating the night sky.",
-  },
-  // Add more photo objects as needed
-];
+import { photos } from "src/data/common";
 
 const HomePage = () => {
   const [opacity, setOpacity] = useState(1);
+  const [scrollY, setScrollY] = useState(0);
 
   const handleScroll = () => {
     const scrollPosition = window.scrollY;
-    const scrollPercentage = (scrollPosition / window.innerHeight) * 100;
+    setScrollY(scrollPosition);
+    const viewportHeight = window.innerHeight;
+    const scrollThreshold = viewportHeight * 0.1; // Adjust the percentage as needed
+
     const newOpacity =
-      scrollPercentage < 40 ? 1 : 1 - (scrollPercentage - 40) / 20; // Adjust the scroll threshold here
+      scrollPosition < scrollThreshold
+        ? 1
+        : 1 - (scrollPosition - scrollThreshold) / (viewportHeight * 0.8);
     setOpacity(newOpacity < 0 ? 0 : newOpacity);
   };
 
@@ -51,40 +40,70 @@ const HomePage = () => {
     cssEase: "linear",
   };
 
+  const parallaxStyle = {
+    transform: `translateY(${scrollY * 0.8}px)`, // Adjust the parallax effect by changing the multiplier
+  };
+
+  const arrows = [
+    { delay: "0.4s" },
+    { delay: "0.2s" },
+    { delay: "0s" },
+    // Add more objects if needed for additional bounces
+  ];
+
   return (
     <div>
       <div className="relative h-screen overflow-hidden">
-        <Slider {...settings} style={{ opacity }}>
-          {photos.map((photo) => (
-            <div key={photo.id}>
-              <img
-                src={photo.imageUrl}
-                alt={photo.title}
-                className="w-full h-screen object-cover"
-              />
-            </div>
-          ))}
-        </Slider>
-        <div className="absolute inset-x-0 bottom-8 flex justify-center">
-          <IoIosArrowDown className="text-white text-4xl animate-bounce" />
+        <div style={parallaxStyle}>
+          <Slider {...settings} style={{ opacity }}>
+            {photos.map((photo) => (
+              <div key={photo.id}>
+                <img
+                  src={photo.imageUrl}
+                  alt={photo.title}
+                  className="w-full h-screen object-cover"
+                />
+              </div>
+            ))}
+          </Slider>
         </div>
+
+        <div className="absolute inset-x-0 bottom-8 flex justify-center items-center">
+          <div className="flex flex-col items-center">
+            {arrows.map(({ delay }, index) => (
+              <div
+                key={index}
+                style={{
+                  position: "absolute",
+                  bottom: `${index * 1}rem`,
+                  animation: `bounce 2s infinite ${delay} cubic-bezier(0.28, 0.84, 0.42, 1)`,
+                }}
+              >
+                <IoIosArrowDown className="text-white text-4xl" />
+              </div>
+            ))}
+          </div>
+        </div>
+
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="container mx-auto text-center text-white">
-            <h1 className="text-4xl font-bold mb-4">
+            <h1 className="text-4xl font-bold mb-4 md:text-6xl">
               Welcome to My Elegant Portfolio
             </h1>
-            <p className="text-lg">Beautiful Moments Captured</p>
+            <p className="text-lg md:text-xl">Beautiful Moments Captured</p>
           </div>
         </div>
       </div>
+
       <div className="container mx-auto py-12">
-        <h2 className="text-2xl font-semibold mb-8">Gallery</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <h2 className="text-2xl font-semibold mb-8 text-center">Gallery</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
           {photos.map((photo) => (
             <PhotoCard key={photo.id} photo={photo} />
           ))}
         </div>
       </div>
+
       <AboutUs />
     </div>
   );
